9eb37abe2e54e805d6c385d152412b4d28a01da5
MohamedEAmer <mohamedamer1101@gmail.com>
2024-03-08 00:09:55 +0200

    first commit

---
+++ report/main.js
@@ -0,0 +1,19 @@
+const sidebar = document.querySelector(".sidebar")
+const info = document.querySelector(".hidden")
+const pro1 = document.querySelector(".counter1")
+const pro2 = document.querySelector(".counter2")
+const pro3 = document.querySelector(".counter3")
+const pro4 = document.querySelector(".counter4")
+const pro5 = document.querySelector(".counter5")
+
+
+
+
+function showSidebar(){
+    sidebar.style.display='flex'
+}
+function hideSidebar(){
+    sidebar.style.display='none'
+
+}
+
